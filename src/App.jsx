3e665baceb71498bd2de6896ac7d6f0e7eedd75d
JGbrowni <ad.oscarflores02@ufg.edu.sv>
2023-10-30 20:02:03 -0600

    fallos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import CarritoPage from "./pages/CarritoPage";
 import AppRouter from "./routes/AppRouters";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <CarritoPage />
       <AppRouter />
     </>
   );
